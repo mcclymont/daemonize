e0a70c760806d743c593feba1432979b60684d90
Chris McClymont <chris@mcclymont.it>
2013-12-03 00:41:40 +0000

    Added to keep_fds functionality
     * added optional close_fds parameters
     * keep_fds and close_fds now can be overloaded with string paths.

--- daemonize.py
+++ daemonize.py
@@ -16,13 +16,21 @@ class Daemonize(object):
     - app: contains the application name which will be sent to syslog.
     - pid: path to the pidfile.
     - action: your custom function which will be executed after daemonization.
-    - keep_fds: optional list of fds which should not be closed.
+    - keep_fds: optional list of fds or file paths which should not be closed.
+    - close_fds: optional list of fds or file paths which should be closed. (not to be
+      used with keep_fds). If close_fds is specified, only those fds will be closed. 
+      Otherwise, all open files will be closed (except ones specified by keep_fds)
     """
-    def __init__(self, app, pid, action, keep_fds=[], logger=None):
+    def __init__(self, app, pid, action, logger=None, keep_fds=[], close_fds=[]):
         self.app = app
         self.pid = pid
         self.action = action
-        self.keep_fds = keep_fds
+        self.keep_fds = keep_fds if keep_fds else []
+        self.close_fds = close_fds if close_fds else []
+        
+        if len(self.keep_fds) > 0 and len(self.close_fds) > 0:
+            raise ArgumentError("keep_fds and close_fds are mutually exclusive")
+        
         # Initialize logging.
         if logger:
             self.logger = logger
@@ -44,6 +52,16 @@ class Daemonize(object):
                                           "%b %e %H:%M:%S")
             syslog.setFormatter(formatter)
             self.logger.addHandler(syslog)
+            
+        self.file_mapping = {}
+        for fd in os.listdir('/proc/self/fd'):
+            fd = int(fd)
+            try:
+                fdpath = os.readlink('/proc/self/fd/%s' % fd)
+                if fdpath != '/proc/self/fd':
+                    self.file_mapping[fdpath] = fd
+            except:
+                pass
 
     def sigterm(self, signum, frame):
         """ sigterm method
@@ -52,7 +70,19 @@ class Daemonize(object):
         self.logger.warn("Caught signal %s. Stopping daemon." % signum)
         os.remove(self.pid)
         sys.exit(0)
-
+        
+    # Convert all paths to file descriptors and remove non existing paths
+    def clean_list(self, files):      
+        # Iterate backwards to allow in-place modification of list
+        for i in reversed(range(len(files))):
+            file = files[i]
+            if type(file) is str:
+                fd = self.file_mapping.get(file)
+                if fd:
+                    files[i] = fd
+                else:
+                    del files[i] # If not found, remove from list
+    
     def start(self):
         """ start method
         Main daemonization process.
@@ -83,17 +113,37 @@ class Daemonize(object):
             # than /dev/null.
             devnull = os.devnull
 
-        if self.keep_fds:
-            for fd in range(resource.getrlimit(resource.RLIMIT_NOFILE)[0]):
-                if fd not in self.keep_fds:
-                    try:
-                        os.close(fd)
-                    except OSError:
-                        pass
 
-        os.open(devnull, os.O_RDWR)
-        os.dup(0)
-        os.dup(0)
+        # If the user supplied values to close_fds, then we will only close those.
+        # Else, we will 'automatically' close all available fds.
+        # Need to check here because clean_list might remove specified paths if they 
+        # don't exist and bring the lists length to 0 (when the user was still 
+        # intending for not every fd to be closed automatically
+        automatic_close = len(self.close_fds) == 0
+    
+        self.clean_list(self.close_fds)
+        self.clean_list(self.keep_fds)
+        self.closed = set()
+    
+        if automatic_close:
+            all_fds = range(resource.getrlimit(resource.RLIMIT_NOFILE)[0])
+            self.close_fds = set([fd for fd in all_fds if fd not in self.keep_fds])
+    
+        for fd in self.close_fds:
+            try:
+                os.close(fd)
+                self.closed.add(fd)
+                self.logger.info("successfully closed " + str(fd))
+            except OSError:
+                pass
+        
+        # All applications must have an open connection to stdin, stdout and stderr
+        # even though by design daemons should not actually write to these places.
+        # Therefore, if we closed their respective fds, reopen to /dev/null.
+        # os.open will open the file and map to the lowest available fd.
+        if 0 in self.closed: os.open(devnull, os.O_RDWR)
+        if 1 in self.closed: os.open(devnull, os.O_RDWR)
+        if 2 in self.closed: os.open(devnull, os.O_RDWR)
 
         # Set umask to default to safe file permissions when running as a root daemon. 027 is an
         # octal number which we are typing as 0o27 for Python3 compatibility.
@@ -118,5 +168,4 @@ class Daemonize(object):
         atexit.register(self.sigterm)
 
         self.logger.warn("Starting daemon.")
-
         self.action()
